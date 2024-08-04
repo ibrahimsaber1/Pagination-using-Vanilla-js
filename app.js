44f8b2f108fff3a9954ae28f2d1c82dfa0b11933
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-04 21:00:26 +0300

    app.js: display 10 items in each page

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // catching our elements :)
 
-const tasklist = document.querySelector("task-list");
+const tasklist = document.querySelector(".task-list");
 
-const first = document.querySelector("first");
-const Previous = document.querySelector("Previous");
-const next = document.querySelector("next");
-const last = document.querySelector("last");
+const first = document.querySelector(".first");
+const Previous = document.querySelector(".Previous");
+const next = document.querySelector(".next");
+const last = document.querySelector(".last");
 
 let arraylist = [];
+let page = 0;
 for (let i = 1; i <= 100; i++) {
   let li = document.createElement("li");
-  li.textContent = `List item number $(i)`;
+  li.textContent = `List item number ${i}`;
   arraylist.push(li);
 }
 
-console.log(arraylist);
+for (let i = 0; i <= page + 10; i++) {
+  tasklist.appendChild(arraylist[i]);
+}
+
+// console.log(arraylist);
